00c7bef59cfb5567bb83fcec564bd84f8e3be8a0
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-16 17:24:31 -0300

    BUGFIXED: Cuando se carga la zona horaria desde la base de datos viene como datetime y rompía, y como no era string retornaba None por el isinstance

--- twitter154/models/common.py
+++ twitter154/models/common.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from pydantic import BaseModel, field_validator
 
@@ -7,9 +7,14 @@ class ValCommonModel(BaseModel):
     creation_date: datetime
 
     @field_validator('creation_date', mode="before")
-    def parse_fecha_creacion(cls, date_str):
-        if isinstance(date_str, str):
+    def parse_fecha_creacion(cls, date: str | datetime):
+        if isinstance(date, str):
             try:
-                return datetime.strptime(date_str, '%a %b %d %H:%M:%S %z %Y')
+                return datetime.strptime(date, '%a %b %d %H:%M:%S %z %Y')
             except ValueError:
-                raise ValueError(f"Invalid format date: {date_str}")
+                raise ValueError(f"Invalid format date: {date}")
+        elif isinstance(date, datetime):
+            return date.replace(tzinfo=timezone.utc)
+        else:
+            raise Exception("Valor Inválido.")
+            
