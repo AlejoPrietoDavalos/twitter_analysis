6244f1d23ab3f85d8983ba33db1d5b755c830704
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-23 18:42:50 -0300

    headers en clase abstracta

--- scraping_kit/bot_scraper.py
+++ scraping_kit/bot_scraper.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Type, List, Tuple, Callable
+from typing import Type, List, Tuple
 from pathlib import Path
 import json
 import requests
@@ -9,14 +9,14 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 from scraping_kit.requests_args import ReqArgs
-
+from scraping_kit.headers import Headers
 
 class BotScraper(BaseModel):
     acc_name: str = Field(frozen=True)
     credential: str = Field(frozen=True, repr=False)
 
-    def get_response(self, req_args: Type[ReqArgs], fn_headers: Callable) -> Tuple[Response, datetime]:
-        response = requests.request(**req_args.response_dump(), headers=fn_headers(self))
+    def get_response(self, req_args: Type[ReqArgs], headers: Type[Headers]) -> Tuple[Response, datetime]:
+        response = requests.request(**req_args.response_dump(), headers=headers.get_header(self))
         date_now = datetime.now()
         return response, date_now
 
