f090229c3939e7d9a447bd36a0f644af3c061b6b
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-23 17:36:52 -0300

    fix @property

--- scraping_kit/db/db_twitter.py
+++ scraping_kit/db/db_twitter.py
@@ -3,37 +3,16 @@ from pathlib import Path
 from pymongo.database import Database
 from pymongo.collection import Collection
 
-from _twitter154.db.base import DBMongoBase, HOST_DEFAULT
+from scraping_kit.db.base import DBMongoBase, HOST_DEFAULT
 
 
 class DBTwitterColl:
     def __init__(self, db: Database):
-        self.__raw: Collection = db["raw"]
-        self.__user: Collection = db["user"]
-        self.__user_suspended: Collection = db["user_suspended"]
-        self.__tweet: Collection = db["tweet"]
-        self.__cursors: Collection = db["cursors"]
-
-    @property
-    def raw(self) -> Collection:
-        return self.__raw
-
-    @property
-    def user(self) -> Collection:
-        return self.__user
-    
-    @property
-    def user_suspended(self) -> Collection:
-        return self.__user_suspended
-
-    @property
-    def tweet(self) -> Collection:
-        return self.__tweet
-    
-    @property
-    def cursors(self) -> Collection:
-        return self.__cursors
-
+        self.raw: Collection = db["raw"]
+        self.user: Collection = db["user"]
+        self.user_suspended: Collection = db["user_suspended"]
+        self.tweet: Collection = db["tweet"]
+        self.cursors: Collection = db["cursors"]
 
 class DBTwitter(DBMongoBase):
     def __init__(self, path_data: Path, db_name: str, host=HOST_DEFAULT):
