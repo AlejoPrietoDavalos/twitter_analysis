f7a44317cd1715759ec3c2975f10c24dfb14447e
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-23 21:11:29 -0300

    BotScraper y DBTwitter con load_bots dado el acc.json

--- scraping_kit/bot_scraper.py
+++ scraping_kit/bot_scraper.py
@@ -1,25 +1,67 @@
 from __future__ import annotations
-from typing import Type, List, Tuple
+from typing import Type, List, Tuple, Literal
 from pathlib import Path
 import json
 import requests
 from requests import Response
 from datetime import datetime
+from abc import ABC, abstractclassmethod
 
 from pydantic import BaseModel, Field
 
-from scraping_kit.requests_args import ReqArgs
-from scraping_kit.headers import Headers
+
+class Headers(ABC):
+    @abstractclassmethod
+    def get_header(cls, bot_scraper: BotScraper) -> dict:
+        """ It must return the header necessary to send the requests to the API."""
+        ...
+
+
+class ReqArgs(BaseModel, ABC):
+    method: Literal["GET", "POST"] = "GET"
+
+    @abstractclassmethod
+    def header_cls(cls) -> Type[Headers]:
+        """ Must return the header class."""
+        ...
+
+    @abstractclassmethod
+    def endpoint_name(cls) -> str:
+        """ Endpoint name."""
+        ...
+
+    @abstractclassmethod
+    def url(cls) -> str:
+        """ Endpoint URL."""
+        ...
+    
+    @classmethod
+    def get_header(cls, bot_scraper: BotScraper) -> dict:
+        header_cls = cls.header_cls()
+        return header_cls.get_header(bot_scraper)
+
+    def response_dump(self) -> dict:
+        model_dump_ = super().model_dump()
+        model_dump_["url"] = self.url()
+        return model_dump_
+
+    def model_dump(self) -> dict:
+        model_dump_ = super().model_dump()
+        model_dump_["endpoint_name"] = self.endpoint_name()
+        model_dump_["url"] = self.url()
+        return model_dump_
+
 
 class BotScraper(BaseModel):
     acc_name: str = Field(frozen=True)
     credential: str = Field(frozen=True, repr=False)
 
-    def get_response(self, req_args: Type[ReqArgs], headers: Type[Headers]) -> Tuple[Response, datetime]:
-        response = requests.request(**req_args.response_dump(), headers=headers.get_header(self))
+    def get_response(self, req_args: Type[ReqArgs]) -> Tuple[Response, datetime]:
+        response = requests.request(**req_args.response_dump(), headers=req_args.get_header(self))
         date_now = datetime.now()
         return response, date_now
 
-def load_bots(path_acc: Path) -> List[BotScraper]:
-    with open(path_acc) as f:
-        return [BotScraper(**acc_json) for acc_json in json.load(f)]
+    @classmethod
+    def load_bots(path_acc: Path) -> List[BotScraper]:
+        with open(path_acc) as f:
+            return [BotScraper(**acc_json) for acc_json in json.load(f)]
--- scraping_kit/db/db_twitter.py
+++ scraping_kit/db/db_twitter.py
@@ -1,31 +1,36 @@
+from typing import List
 from pathlib import Path
 
 from pymongo.database import Database
 from pymongo.collection import Collection
 
 from scraping_kit.db.base import DBMongoBase, HOST_DEFAULT
+from scraping_kit.bot_scraper import load_bots, BotScraper
 
 
 class DBTwitterColl:
     def __init__(self, db: Database):
-        self.raw: Collection = db["raw"]
-        self.user: Collection = db["user"]
-        self.user_suspended: Collection = db["user_suspended"]
-        self.tweet: Collection = db["tweet"]
-        self.cursors: Collection = db["cursors"]
+        self.raw: Collection = db.get_collection("raw")
+        self.user: Collection = db.get_collection("user")
+        self.user_suspended: Collection = db.get_collection("user_suspended")
+        self.tweet: Collection = db.get_collection("tweet")
+        self.cursors: Collection = db.get_collection("cursors")
+
 
 class DBTwitter(DBMongoBase):
     def __init__(self, path_data: Path, db_name: str, host=HOST_DEFAULT):
         super().__init__(db_name=db_name, host=host)
-        self.__coll = DBTwitterColl(db = self.db)
-        self.__init_db(path_data=path_data)
-    
-    @property
-    def coll(self) -> DBTwitterColl:
-        return self.__coll
-    
-    def __init_db(self, path_data: Path) -> None:
-        path_data.mkdir(exist_ok=True)
+        self.coll = DBTwitterColl(db = self.db)
         
+        self.path_data = path_data
+        self.path_reports_folder = self.path_data / "reports"
+        self.path_acc = self.path_data / "acc.json"
+        self.__init_db()
+    
+    def load_bots(self) -> List[BotScraper]:
+        return BotScraper.load_bots(self.path_acc)
 
+    def __init_db(self) -> None:
+        self.path_data.mkdir(exist_ok=True)
+        self.path_reports_folder.mkdir(exist_ok=True)
 
