bf613c6a45a2104cbb642d562a9f286dfadbedae
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-27 21:01:32 -0300

    trends

--- scraping_kit/utils.py
+++ scraping_kit/utils.py
@@ -1,5 +1,5 @@
 from typing import Tuple, Dict
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
 
 from scraping_kit.db.db_twitter import DBTwitter
@@ -19,3 +19,7 @@ def load_db_and_bots(path_data: Path, db_name: str, verbose=True) -> Tuple[DBTwi
 def date_delta(date_i: datetime, date_f: datetime) -> Dict[str, datetime]:
     return {"$gte": date_i, "$lt": date_f}
 
+def date_one_day(year: int, month: int, day: int) -> Dict[str, datetime]:
+    date_i = datetime(year, month, day, tzinfo=timezone.utc)
+    date_f = date_i + timedelta(days=1)
+    return date_delta(date_i=date_i, date_f=date_f)
