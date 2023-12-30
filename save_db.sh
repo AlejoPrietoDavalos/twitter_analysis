7aec78ec84a62f5074013489e1f176980979385a
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-12-30 14:24:08 -0300

    delete

--- save_db.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mongodump --db scrape_tw --out data/backup_db/date_$(date +"%Y_%m_%d_%H_%M_%S")
--- user_test.py
+++
@@ -1,5 +0,0 @@
-def get_test_uid() -> str:
-    return "3012852462"
-
-def get_test_screen_name() -> str:
-    return "zachxbt"
