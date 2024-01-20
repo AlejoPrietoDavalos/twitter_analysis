1440a6dd8df0e23116d8d8e8a4cb564c8ea0a0f2
Alejo Prieto <alejo.prieto@extendeal.com>
2024-01-20 19:19:42 -0300

    collect_trends

--- twitter_trends/functional.py
+++ twitter_trends/functional.py
@@ -5,6 +5,7 @@ from pymongo.results import InsertOneResult
 from scraping_kit import DBTwitter, BotScraper, date_one_day
 from scraping_kit.db.models.raw import RawData
 from scraping_kit.db.models.trends import Trends
+from scraping_kit.utils import format_yyyy_xx
 from twitter_trends.params import ArgsTwitterTrends
 
 
@@ -16,22 +17,25 @@ def process_trends_raw_data(db_tw: DBTwitter, insert_one_result_raw: InsertOneRe
     return insert_one_result_trend
 
 
-def requests_and_process(db_tw: DBTwitter, req_args: ArgsTwitterTrends, bot: BotScraper) -> InsertOneResult:
+def requests_and_process(db_tw: DBTwitter, bot: BotScraper, woeid: str = None) -> InsertOneResult:
     date_now = datetime.now().date()
     date_now = date_one_day(date_now.year, date_now.month, date_now.day)
     trends = db_tw.coll.trends_from_date_range(date_now)
     # FIXME: Revisa que sea el primer trend, y sino retorna None.
 
     if trends is not None:
-        _d = trends.created
         msg = f"You already have a Trends on the date: "
-        year, month, day = str(_d.year).zfill(4), str(_d.month).zfill(2), str(_d.day).zfill(2)
-        hour, minute, second = str(_d.hour).zfill(2), str(_d.minute).zfill(2), str(_d.second).zfill(2)
-        msg += f"{year}-{month}-{day} {hour}:{minute}:{second}"
+        d = trends.created
+        d = format_yyyy_xx(*d.timetuple()[:6]).split("_")
+        format_d = lambda yyyy,mm,dd,HH,MM,SS: f"{yyyy}-{mm}-{dd} {HH}:{MM}:{SS}"
+        d = format_d(*d)
+        
+        msg += d
         print(msg)
         print("You don't need to download again.")
         return None
     else:
+        req_args = ArgsTwitterTrends.from_woeid(woeid)
         insert_one_result_raw = db_tw.requests_and_save(req_args, bot)
         insert_one_result_trend = process_trends_raw_data(db_tw, insert_one_result_raw)
         db_tw.coll.change_is_processed(insert_one_result_raw)
