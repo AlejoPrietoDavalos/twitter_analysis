c47ee56c02794263bd8272546e26d5db7b25eb6c
Alejo Prieto <alejo.prieto@extendeal.com>
2024-01-20 19:21:14 -0300

    Se settea el loader por defecto.

--- scraping_kit/utils_loader.py
+++ scraping_kit/utils_loader.py
@@ -3,7 +3,13 @@ from pathlib import Path
 from scraping_kit.db.db_twitter import DBTwitter
 from scraping_kit.bot_scraper import BotList
 
-def load_db_and_bots(path_data: Path, db_name: str, verbose=True) -> Tuple[DBTwitter, BotList]:
+def load_db_and_bots(
+        path_data: Path = None,
+        db_name: str = "scrape_tw",
+        verbose=True
+    ) -> Tuple[DBTwitter, BotList]:
+    if path_data is None:
+        path_data = Path("data")
     db_tw = DBTwitter(path_data, db_name)
     bots = db_tw.load_bots()
     if verbose:
