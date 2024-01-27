31867257cd5f4c0877ba3a191630c3959fa4bb23
Alejo Prieto <alejo.prieto@extendeal.com>
2024-01-27 09:56:55 -0300

    Save and Backup db without dirty outputs

--- scraping_kit/utils_loader.py
+++ scraping_kit/utils_loader.py
@@ -1,8 +1,12 @@
 from typing import Tuple, List
+from datetime import datetime
 from pathlib import Path
+import subprocess
+import os
 
 import pandas as pd
 
+from scraping_kit.utils import format_yyyy_xx
 from scraping_kit.db.db_twitter import DBTwitter
 from scraping_kit.bot_scraper import BotList
 
@@ -20,6 +24,7 @@ def load_db_and_bots(
         print(f"Bots: {bots}")
     return db_tw, bots
 
+
 def load_profiles(path_data: Path = None) -> List[str]:
     """ TODO: Ask Vera how she wants to import the users to study."""
     if path_data is None:
@@ -28,3 +33,26 @@ def load_profiles(path_data: Path = None) -> List[str]:
     df = df.sort_values("followersCount", ascending=False)
     profiles = df["screenName"].to_list()
     return profiles
+
+def save_db(db_name: str = "scrape_tw", folder_backup: str = "data/backup_db") -> None:
+    date_now = datetime.now()
+    date_now = format_yyyy_xx(*date_now.timetuple()[:6])
+    path_out_db = f"{folder_backup}/date_{date_now}"
+    process = subprocess.run(
+        ["mongodump", "--db", db_name, "--out", path_out_db],
+        stderr = subprocess.DEVNULL,
+        stdin = subprocess.DEVNULL
+    )
+    if process.returncode == 0:
+        print(f"Database saved into: {path_out_db}")
+    else:
+        print("Failed to connect to server, run_server.")
+
+def run_server(path_db: str = "~/.data/db") -> None:
+    subprocess.Popen(
+        f"mongod --dbpath '{path_db}'",
+        shell = True,
+        stdin = subprocess.DEVNULL,
+        stdout = subprocess.DEVNULL
+    )
+    print("Server running. You can review it by entering: http://localhost:27017/")
